ce601eba1a41c001e8b6e6a3d7cda2f4b4325ef9
SolomonMuhye <solomon.muhye.wd@gmail.com>
2023-07-19 18:03:12 +0300

    finishing the hero section

---
+++ src/components/Hero.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function Hero() {
+  return (
+    <div className='container mx-auto text-center w-3/4 p-20'>
+      <h1 className='text-brightRed text-7xl font-bold mb-12'>Our Services</h1>
+      <p className='leading-8'>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Odit, incidunt
+        vitae fugit harum architecto doloribus facere omnis recusandae pariatur
+        porro quis alias quas animi, dignissimos fuga ab reprehenderit
+        repudiandae ea?
+      </p>
+    </div>
+  );
+}
