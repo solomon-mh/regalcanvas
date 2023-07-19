20bcb62d764b64f3031cccedbc6a59573ee39bfe
SolomonMuhye <solomon.muhye.wd@gmail.com>
2023-07-20 02:08:49 +0300

    finishing the product  section

---
+++ src/components/sub/Products.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+export default function Products() {
+  return (
+    <div className='flex space-x-6 justify-center items-center mx-12 flex-wrap'>
+      <div className='my-8 mx-4 w-30perc'>
+        <p className='text-7xl text-brightRed font-bold'>&#9813;</p>
+        <h1 className='text-xl font-bold my-6'>Interface Design</h1>{" "}
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deleniti
+          voluptatem reiciendis minus, a dolores.
+        </p>
+      </div>
+      <div className='my-8 mx-4 w-30perc'>
+        <p className='text-7xl text-brightRed font-bold'>&#9921;</p>
+        <h1 className='text-xl font-bold my-6'>Product Design</h1>{" "}
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deleniti
+          voluptatem reiciendis minus, a dolores.
+        </p>
+      </div>
+      <div className='my-8 mx-4 w-30perc'>
+        <p className='text-7xl text-brightRed font-bold'>&#9965;</p>
+        <h2 className='text-xl font-bold my-6'>Quality Results</h2>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deleniti
+          voluptatem reiciendis minus, a dolores.
+        </p>
+      </div>
+    </div>
+  );
+}
