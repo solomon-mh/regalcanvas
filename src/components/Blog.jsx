615014e0edb75df85b362c62271dd23046d0a04a
SolomonMuhye <solomon.muhye.wd@gmail.com>
2023-07-20 10:48:02 +0300

    working on Blog section

---
+++ src/components/Blog.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function Blog() {
+  return (
+    <div className='container mx-auto text-center w-3/4 p-20'>
+      <h1 className='text-brightRed text-6xl font-bold font-sans mb-12'>
+        Blog Updates
+      </h1>
+      <p className='leading-8'>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Odit, incidunt
+        vitae fugit harum architecto doloribus facere omnis recusandae pariatur
+        porro quis alias quas animi, dignissimos fuga ab reprehenderit
+        repudiandae ea?
+      </p>
+    </div>
+  );
+}
