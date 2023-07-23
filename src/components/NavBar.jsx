81687165f1f6d414b7a6dbf2bc2aad932eeaf795
SolomonMuhye <solomon.muhye.wd@gmail.com>
2023-07-23 11:40:44 +0300

    making the navbar responsive

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,29 +1,173 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import "../styles/navbar.css";
+
 export default function NavBar() {
+  const [activeLink, setActiveLink] = useState("/");
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const handleLinkClick = (to) => {
+    setActiveLink(to);
+    setMenuOpen(false);
+  };
+
+  const handleMenuToggle = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
-    <ul className='flex gap-8 justify-end w-full p-12 pr-32 '>
-      <li className='mr-auto ml-14 text-brightRed font-bold cursor-pointer'>
-        MONARCHY
-      </li>
-      <Link to='/'>
-        <li>Home</li>
-      </Link>
-      <Link to='/Portfolio'>
-        <li>Portfolio</li>
-      </Link>
-      <Link to='/About'>
-        <li>About</li>
-      </Link>
-      <Link to='/Service'>
-        <li>Service</li>
-      </Link>
-      <Link to='/Blog'>
-        <li>Blog</li>
-      </Link>
-      <Link to='/Contact'>
-        <li>Contact</li>
-      </Link>
-    </ul>
+    <nav className='flex gap-8 justify-end w-full px-6 lg:p-12 lg:pr-24 '>
+      <div className='container mx-auto px-4 md:px-8'>
+        <div className='flex justify-between items-center h-16'>
+          <div className='mr-auto text-brightRed font-bold cursor-pointer'>
+            MONARCHY
+          </div>
+          {/* Hamburger menu icon */}
+          <div className='md:hidden'>
+            <button
+              onClick={handleMenuToggle}
+              className='flex items-center px-3 ml-3 -mr-3 py-2 border rounded text-brightRed border-brightRed hover:text-white hover:bg-brightRed focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-white focus:ring-brightRed'
+            >
+              <svg
+                className='h-4 w-4 fill-current'
+                viewBox='0 0 20 20'
+                xmlns='http://www.w3.org/2000/svg'
+              >
+                <title>Menu</title>
+                {menuOpen ? (
+                  <path
+                    fillRule='evenodd'
+                    clipRule='evenodd'
+                    d='M4 5h12a1 1 0 010 2H4a1 1 0 010-2zm0 5h12a1 1 0 010 2H4a1 1 0 010-2zm0 5h12a1 1 0 010 2H4a1 1 0 010-2z'
+                  />
+                ) : (
+                  <path
+                    fillRule='evenodd'
+                    clipRule='evenodd'
+                    d='M2 4.267a1 1 0 011.243-.97l14 4A1 1 0 0118 9.333v1.334a1 1 0 01-.757.97l-14 4A1 1 0 012 15.733V4.267zm15 5.466l-14-4v6.534l14 4V9.733z'
+                  />
+                )}
+              </svg>
+            </button>
+          </div>
+          {/* Desktop navigation */}
+          <div className={`hidden md:flex md:space-x-8 items-center`}>
+            <Link
+              className={`nav-links ${activeLink === "/" ? "active-link" : ""}`}
+              to='/'
+              onClick={() => handleLinkClick("/")}
+            >
+              Home
+            </Link>
+            <Link
+              className={`nav-links ${
+                activeLink === "/Portfolio" ? "active-link" : ""
+              }`}
+              to='/Portfolio'
+              onClick={() => handleLinkClick("/Portfolio")}
+            >
+              Portfolio
+            </Link>
+            <Link
+              className={`nav-links ${
+                activeLink === "/About" ? "active-link" : ""
+              }`}
+              to='/About'
+              onClick={() => handleLinkClick("/About")}
+            >
+              About
+            </Link>
+            <Link
+              className={`nav-links ${
+                activeLink === "/Service" ? "active-link" : ""
+              }`}
+              to='/Service'
+              onClick={() => handleLinkClick("/Service")}
+            >
+              Services
+            </Link>
+            <Link
+              className={`nav-links ${
+                activeLink === "/Blog" ? "active-link" : ""
+              }`}
+              to='/Blog'
+              onClick={() => handleLinkClick("/Blog")}
+            >
+              Blog
+            </Link>
+            <Link
+              className={`nav-links ${
+                activeLink === "/Contact" ? "active-link" : ""
+              }`}
+              to='/Contact'
+              onClick={() => handleLinkClick("/Contact")}
+            >
+              Contact
+            </Link>
+          </div>
+        </div>
+      </div>
+      {/* Mobile menu */}
+      {menuOpen && (
+        <div className='md:hidden ml-1 px-12 relative mx-0 bg-white'>
+          <div className='px-2 w-48 py-4 space-y-1'>
+            <Link
+              className={`block px-3 py-2 rounded-md text-base font-medium ${
+                activeLink === "/" ? "active-link-sm" : ""
+              }`}
+              to='/'
+              onClick={() => handleLinkClick("/")}
+            >
+              Home
+            </Link>
+            <Link
+              className={`block px-3 py-2 rounded-md text-base font-medium ${
+                activeLink === "/Portfolio" ? "active-link-sm" : ""
+              }`}
+              to='/Portfolio'
+              onClick={() => handleLinkClick("/Portfolio")}
+            >
+              Portfolio
+            </Link>
+            <Link
+              className={`block px-3 py-2 rounded-md text-base font-medium ${
+                activeLink === "/About" ? "active-link-sm" : ""
+              }`}
+              to='/About'
+              onClick={() => handleLinkClick("/About")}
+            >
+              About
+            </Link>
+            <Link
+              className={`block px-3 py-2 rounded-md text-base font-medium ${
+                activeLink === "/Service" ? "active-link-sm" : ""
+              }`}
+              to='/Service'
+              onClick={() => handleLinkClick("/Service")}
+            >
+              Services
+            </Link>
+            <Link
+              className={`block px-3 py-2 rounded-md text-base font-medium ${
+                activeLink === "/Blog" ? "active-link-sm" : ""
+              }`}
+              to='/Blog'
+              onClick={() => handleLinkClick("/Blog")}
+            >
+              Blog
+            </Link>
+            <Link
+              className={`block px-3 py-2 rounded-md text-base font-medium ${
+                activeLink === "/Contact" ? "active-link-sm" : ""
+              }`}
+              to='/Contact'
+              onClick={() => handleLinkClick("/Contact")}
+            >
+              Contact
+            </Link>
+          </div>
+        </div>
+      )}
+    </nav>
   );
 }
