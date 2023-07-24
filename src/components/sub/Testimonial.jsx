746e2c6803bb51b626a2530f7c626bd0e1ca9d27
SolomonMuhye <solomon.muhye.wd@gmail.com>
2023-07-25 01:53:26 +0300

    some final modfication

--- src/components/sub/Testimonial.jsx
+++ src/components/sub/Testimonial.jsx
@@ -41,8 +41,10 @@ const Testimonials = [
 
 export default function Testimonial() {
   return (
-    <div className='text-center bg-white py-32'>
-      <h1 className='text-4xl text-black font-sans py-20 '>Testimonials</h1>
+    <div className='text-center bg-white py-6 lg:pt-4 pb-24'>
+      <h1 className='text-4xl text-black font-sans pt-16 pb-28 '>
+        Testimonials
+      </h1>
       <div
         className='text-left grid grid-cols-1 lg:gap-8 lg:grid-cols-4 sm:px-12 box-border justify-center'
         lg:style={{ gridTemplateColumns: "repeat(4, 20%)" }}
