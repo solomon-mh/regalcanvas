3e3dd4a814323fe0377e57e122e087d30108e238
SolomonMuhye <solomon.muhye.wd@gmail.com>
2023-10-30 11:14:16 +0300

    using layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,31 @@
-import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import PortfolioDescription from "./components/sub/PortfolioDescription";
-import NavBar from "./components/NavBar";
+import Layout from "./components/Layout";
+import Home from "./components/Home";
 import Contact from "./components/Contact";
 import About from "./components/About";
 import Portfolio from "./components/Portfolio";
+import PortfolioDescription from "./components/sub/PortfolioDescription";
 import Service from "./components/Service";
-import Home from "./components/Home";
 import Blog from "./components/Blog";
+import PageNotFound from "./components/404";
 
 function App() {
   return (
     <Router>
-      <NavBar />
       <Routes>
-        <Route path='/Contact' element={<Contact />} />
-        <Route path='/About' element={<About />} />
-        <Route path='/Portfolio' element={<Portfolio />} />
-        <Route path='/' element={<Home />} />
-        <Route path='/Service' element={<Service />} />
-        <Route path='/Blog' element={<Blog />} />
-        <Route
-          path='/PortfolioDescription/:id'
-          element={<PortfolioDescription />}
-        />
+        <Route path='/' element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path='Contact' element={<Contact />} />
+          <Route path='About' element={<About />} />
+          <Route path='Portfolio' element={<Portfolio />} />
+          <Route path='Service' element={<Service />} />
+          <Route path='Blog' element={<Blog />} />
+          <Route
+            path='PortfolioDescription/:id'
+            element={<PortfolioDescription />}
+          />
+          <Route path='*' element={<PageNotFound />} />
+        </Route>
       </Routes>
     </Router>
   );
