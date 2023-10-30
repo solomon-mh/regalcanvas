428d35850d9ae767409e409fa9c65ff27ea4e144
SolomonMuhye <solomon.muhye.wd@gmail.com>
2023-10-30 10:58:49 +0300

    adding data folder for data store

---
+++ src/data/testimonial.js
@@ -0,0 +1,39 @@
+import person1 from "../assets/person_1.jpg";
+import person2 from "../assets/person_2.jpg";
+import person3 from "../assets/person_3.jpg";
+import person4 from "../assets/person_4.jpg";
+
+export const Testimonials = [
+  {
+    id: 1,
+    img: person1,
+    name: "Chad Hawkins",
+    catagory: "Customer",
+    testimony:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui consequuntur rerum, iusto accusamus, numquam et vero aliquam delectus accusantium quo!",
+  },
+  {
+    id: 2,
+    img: person2,
+    name: "Ayisha Atherton",
+    catagory: "Customer",
+    testimony:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui consequuntur rerum, iusto accusamus, numquam et vero aliquam delectus accusantium quo!",
+  },
+  {
+    id: 3,
+    img: person3,
+    name: "Jasleen Dunkley",
+    catagory: "Customer",
+    testimony:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui consequuntur rerum, iusto accusamus, numquam et vero aliquam delectus accusantium quo!",
+  },
+  {
+    id: 4,
+    img: person4,
+    name: "Riccardo Gilliam",
+    catagory: "Customer",
+    testimony:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui consequuntur rerum, iusto accusamus, numquam et vero aliquam delectus accusantium quo!",
+  },
+];
