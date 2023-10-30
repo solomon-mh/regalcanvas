056c43834399991c727a9719652c3ca0ad404932
SolomonMuhye <solomon.muhye.wd@gmail.com>
2023-10-30 11:04:33 +0300

    modifying the BlogUpdates.jsx

--- src/components/sub/BlogUpdates.jsx
+++ src/components/sub/BlogUpdates.jsx
@@ -1,8 +1,7 @@
-import React from "react";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import { portfolioObj } from "./PortfolioData";
+import { portfolioObj } from "../../data/PortfolioData";
 import person1 from "../../assets/person_1.jpg";
 
 export default function BlogUpdates() {
