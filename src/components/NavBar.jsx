e6f67618e7eb026a87b49061455b396f6054d1b8
SolomonMuhye <solomon.muhye.wd@gmail.com>
2024-03-20 23:41:32 +0300

    some mode on Some Nav Icons

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
 import "../styles/navbar.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faNavicon, faTimes } from "@fortawesome/free-solid-svg-icons";
 
 export default function NavBar() {
   const [activeLink, setActiveLink] = useState("/");
@@ -29,28 +31,13 @@ export default function NavBar() {
           <div className='md:hidden'>
             <button
               onClick={handleMenuToggle}
-              className='flex items-center px-3 mt-6 py-2 border rounded text-brightRed border-brightRed hover:text-white hover:bg-brightRed focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-white focus:ring-brightRed'
+              className='flex items-center px-3 mt-6 py-2 border rounded text-brightRed hover:text-white hover:bg-brightRed focus:outline-none '
             >
-              <svg
-                className='h-4 w-4 fill-current'
-                viewBox='0 0 20 20'
-                xmlns='http://www.w3.org/2000/svg'
-              >
-                <title>Menu</title>
-                {menuOpen ? (
-                  <path
-                    fillRule='evenodd'
-                    clipRule='evenodd'
-                    d='M4 5h12a1 1 0 010 2H4a1 1 0 010-2zm0 5h12a1 1 0 010 2H4a1 1 0 010-2zm0 5h12a1 1 0 010 2H4a1 1 0 010-2z'
-                  />
-                ) : (
-                  <path
-                    fillRule='evenodd'
-                    clipRule='evenodd'
-                    d='M2 4.267a1 1 0 011.243-.97l14 4A1 1 0 0118 9.333v1.334a1 1 0 01-.757.97l-14 4A1 1 0 012 15.733V4.267zm15 5.466l-14-4v6.534l14 4V9.733z'
-                  />
-                )}
-              </svg>
+              {menuOpen ? (
+                <FontAwesomeIcon icon={faTimes} />
+              ) : (
+                <FontAwesomeIcon icon={faNavicon} />
+              )}
             </button>
           </div>
           {/* Desktop navigation */}
@@ -112,7 +99,7 @@ export default function NavBar() {
       </div>
       {/* Mobile menu */}
       {menuOpen && (
-        <div className='md:hidden pr-12 relative bg-white mx-0'>
+        <div className='md:hidden pr-12m relative bg-white mx-0'>
           <div className='px-2 w-48 py-4 space-y-1'>
             <Link
               className={`block px-3 py-2 rounded-md text-base font-medium ${
