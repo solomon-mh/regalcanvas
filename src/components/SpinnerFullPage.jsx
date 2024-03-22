0b398ce75f860ab7fb0da2c659886cae324e5d62
SolomonMuhye <solomon.muhye.wd@gmail.com>
2024-03-22 11:55:05 +0300

    adding new section SpinnerFullPage.jsx

---
+++ src/components/SpinnerFullPage.jsx
@@ -0,0 +1,11 @@
+import Spinner from "./Spinner";
+import "../styles/spinner.css";
+function SpinnerFullPage() {
+  return (
+    <div className='spinnerFullpage'>
+      <Spinner />
+    </div>
+  );
+}
+
+export default SpinnerFullPage;
