6fe63c20cca7f3281adf25ae3a63b18235fdf68d
SolomonMuhye <solomon.muhye.wd@gmail.com>
2024-03-23 09:19:20 +0300

    some mode

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,37 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Layout from "./components/Layout";
-import Home from "./pages/Home";
-import Contact from "./pages/Contact";
-import About from "./pages/About";
-import Portfolio from "./pages/Portfolio";
 import PortfolioDescription from "./components/sub/PortfolioDescription";
-import Service from "./pages/Service";
-import Blog from "./pages/Blog";
-import PageNotFound from "./pages/404";
+import SpinnerFullPage from "./components/SpinnerFullPage";
+// pages
+import { Suspense, lazy } from "react";
+const Home = lazy(() => import("./pages/Home"));
+const Contact = lazy(() => import("./pages/Contact"));
+const About = lazy(() => import("./pages/About"));
+const Portfolio = lazy(() => import("./pages/Home"));
+const Service = lazy(() => import("./pages/Service"));
+const Blog = lazy(() => import("./pages/Blog"), 4000);
+const PageNotFound = lazy(() => import("./pages/404"));
 
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path='/' element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path='Contact' element={<Contact />} />
-          <Route path='About' element={<About />} />
-          <Route path='Portfolio' element={<Portfolio />} />
-          <Route path='Service' element={<Service />} />
-          <Route path='Blog' element={<Blog />} />
-          <Route
-            path='PortfolioDescription/:id'
-            element={<PortfolioDescription />}
-          />
-          <Route path='*' element={<PageNotFound />} />
-        </Route>
-      </Routes>
+      <Suspense fallback={<SpinnerFullPage />}>
+        <Routes>
+          <Route path='/' element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path='Contact' element={<Contact />} />
+            <Route path='About' element={<About />} />
+            <Route path='Portfolio' element={<Portfolio />} />
+            <Route path='Service' element={<Service />} />
+            <Route path='Blog' element={<Blog />} />
+            <Route
+              path='PortfolioDescription/:id'
+              element={<PortfolioDescription />}
+            />
+            <Route path='*' element={<PageNotFound />} />
+          </Route>
+        </Routes>
+      </Suspense>
     </Router>
   );
 }
