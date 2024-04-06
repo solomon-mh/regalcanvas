2e23bead489c37344c1de9f2b7220644eb285b91
SolomonMuhye <solomon.muhye.wd@gmail.com>
2024-04-06 04:19:42 +0300

    adding new util section ScrollToTOp.jsx to Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,14 +2,16 @@ import Hero from "../components/Hero";
 import SiteSection from "../components/SiteSection";
 import BlogUpdates from "../components/sub/BlogUpdates";
 import Sites from "../components/sub/Sites";
+import ScrollToTop from "../utils/ScrollToTop";
 
 export default function Home() {
   return (
-    <main>
+    <main id='home'>
       <Hero />
       <SiteSection />
       <BlogUpdates />
       <Sites />
+      <ScrollToTop />
     </main>
   );
 }
