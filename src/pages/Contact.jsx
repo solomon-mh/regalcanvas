93c928afd325999c9ef1cce6c6da6fc324fa130a
SolomonMuhye <solomon.muhye.wd@gmail.com>
2024-04-10 01:52:10 +0300

    cleaning the styling using  @apply

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -29,9 +29,7 @@ export default function Contact() {
   return (
     <div>
       <div className='hero_div'>
-        <h1 className='text-brightRed text-2xl sm:text-4xl lg:text-6.5xl leading-loose word-spacing-4 lg:px-24 font-bold font-sans mb-12'>
-          Contact Us
-        </h1>
+        <h1 className='hero_header'>Contact Us</h1>
         <p className='leading-8 hidden sm:block'>
           Lorem, ipsum dolor sit amet consectetur adipisicing elit. Odit,
           incidunt vitae fugit harum architecto doloribus facere omnis
