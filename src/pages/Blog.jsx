fe4d504aa167453f9279c909fe857e0bfd41aa84
SolomonMuhye <solomon.muhye.wd@gmail.com>
2024-04-10 01:40:51 +0300

    cleaning the styling using  @apply

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -11,7 +11,7 @@ const data = {
 export default function Blog() {
   return (
     <div>
-      <div className='container mx-auto text-center w-full  lg:w-3/4 p-6 md:p-12 lg:p-20 mb-28'>
+      <div className='hero_div'>
         <h1 className='text-brightRed text-2xl sm:text-4xl lg:text-6.5xl leading-loose word-spacing-4 lg:px-24 font-bold font-sans mb-12'>
           Blog Updates
         </h1>
